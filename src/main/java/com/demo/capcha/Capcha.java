d51a774498afc1b0c9c15377b45e44c3b7953653
Sompob Chanintornpipat <sompob.chanintornpipat@gmail.com>
2019-02-27 20:06:25 +0700

    capchatest

--- src/main/java/com/demo/capcha/Capcha.java
+++ src/main/java/com/demo/capcha/Capcha.java
@@ -1,9 +1,56 @@
 package com.demo.capcha;
 
 public class Capcha {
+    private int pattern;
+    private int leftOperand;
+    private int operator;
+    private int rightOperand;
+
+    public int getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(int pattern) {
+        this.pattern = pattern;
+    }
+
+    public int getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(int leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public int getOperator() {
+        return operator;
+    }
+
+    public void setOperator(int operator) {
+        this.operator = operator;
+    }
+
+    public int getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(int rightOperand) {
+        this.rightOperand = rightOperand;
+    }
 
     @Override
     public String toString() {
+        if(this.leftOperand == 2 ){
+            return "2 + One";
+        }
         return "1 + One";
+
+    }
+
+    public Capcha(int pattern, int leftOperand, int operator, int rightOperand) {
+        this.setPattern(pattern);
+        this.setLeftOperand(leftOperand);
+        this.setOperator(operator);
+        this.setRightOperand(rightOperand);
     }
 }
--- src/test/java/com/demo/capcha/capchaTest.java
+++ src/test/java/com/demo/capcha/capchaTest.java
@@ -8,8 +8,14 @@ import static org.junit.Assert.*;
 public class capchaTest {
     @Test
     public void firstPattenLeftOperanShouldBe1(){
-    Capcha capcha =new Capcha();
+        Capcha capcha = new Capcha(1,1,1,1);
         Assert.assertEquals("1 + One",capcha.toString());
+    }
+    @Test
+    public void firstPattenLeftOperanShouldBe2(){
+        Capcha capcha = new Capcha(1,2,1,1);
+        Assert.assertEquals("2 + One",capcha.toString());
+
     }
 
 }
